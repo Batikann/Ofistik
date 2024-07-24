00e4242463053a7bdd906cffaa1ae27dbe2ddfc6
Batikann <emiruar123@gmail.com>
2024-07-24 20:39:44 +0300

    first commit

---
+++ pages/api/profile/[id]/get-profile.js
@@ -0,0 +1,56 @@
+import prisma from '@/lib/prisma'
+
+const handler = async (req, res) => {
+  if (req.method === 'GET') {
+    const { id } = req.query
+
+    if (!id) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'ID parametresi gereklidir',
+      })
+    }
+
+    const include = {
+      user: true, // User bilgilerini çekmek için
+      educationInfo: true, // EducationInfo bilgilerini çekmek için
+      certificates: true, // Certificate bilgilerini çekmek için
+      comments: true,
+      resume: true,
+      skills: true,
+      languages: true,
+    }
+
+    try {
+      const serviceProvider = await prisma.hizmetVeren.findUnique({
+        where: { id: id },
+        include: include,
+      })
+
+      if (!serviceProvider) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'HizmetVeren bulunamadı',
+        })
+      }
+
+      return res.status(200).json({
+        status: 'success',
+        data: serviceProvider,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        status: 'error',
+        message: 'Veritabanında bir hata oluştu!',
+        error: error.message,
+      })
+    }
+  }
+
+  return res.status(405).json({
+    status: 'error',
+    message: 'Method not allowed',
+  })
+}
+
+export default handler
---
+++ pages/api/profile/general/add-resume.js
@@ -0,0 +1,45 @@
+import prisma from '@/lib/prisma'
+
+const handler = async (req, res) => {
+  if (!req) {
+    return res
+      .status(500)
+      .json({ status: 'error', message: 'Bir hata oluştu!' })
+  }
+
+  if (req.method === 'POST' && req.body) {
+    const { resumeText, hizmetVerenId } = req.body
+
+    try {
+      // Check if resume already exists
+      const existingResume = await prisma.resume.findUnique({
+        where: { hizmetVerenId },
+      })
+
+      let result
+      if (existingResume) {
+        // Update existing resume
+        result = await prisma.resume.update({
+          where: { hizmetVerenId },
+          data: { text: resumeText },
+        })
+        return res.status(200).json({ status: 'UPDATED', data: result })
+      } else {
+        // Create new resume
+        result = await prisma.resume.create({
+          data: { text: resumeText, hizmetVerenId },
+        })
+        return res.status(200).json({ status: 'ADDED', data: result })
+      }
+    } catch (error) {
+      console.error('Database request failed:', error)
+      return res.status(500).json({ status: 'error', message: error.message })
+    }
+  } else {
+    return res
+      .status(405)
+      .json({ status: 'error', message: 'Method Not Allowed' })
+  }
+}
+
+export default handler
